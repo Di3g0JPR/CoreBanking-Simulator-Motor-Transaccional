3b6df03b33fca36570903948fa4688d01f0cfa6d
Di3g0JPR <145060581+Di3g0JPR@users.noreply.github.com>
2026-05-31 17:59:03 -0300

    Update AuditoriaDB.java

--- AuditoriaDB.java
+++ AuditoriaDB.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 public class AuditoriaDB {
 
     public static void registrarOperacion(String cliente, double monto, String tipoOperacion) {
-        // El bloque try-with-resources cierra la conexión automáticamente al terminar
+       
         try (Connection conexion = ConexionDB.conectar()) {
             
-            // 1. Nos aseguramos de que la tabla exista en MySQL
+         
             Statement stmt = conexion.createStatement();
             String sqlTabla = "CREATE TABLE IF NOT EXISTS transacciones (" +
                               "id_transaccion INT AUTO_INCREMENT PRIMARY KEY, " +
@@ -19,16 +19,16 @@ public class AuditoriaDB {
                               "fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
             stmt.execute(sqlTabla);
             
-            // 2. Preparamos el comando INSERT de forma segura (los signos ? son los espacios a rellenar)
+            
             String sqlInsert = "INSERT INTO transacciones (cliente, monto, tipo_operacion) VALUES (?, ?, ?)";
             PreparedStatement pstmt = conexion.prepareStatement(sqlInsert);
             
-            // 3. Rellenamos los datos de forma segura
+
             pstmt.setString(1, cliente);
             pstmt.setDouble(2, monto);
             pstmt.setString(3, tipoOperacion);
             
-            // 4. Ejecutamos el envío a la base de datos
+            
             pstmt.executeUpdate();
             System.out.println("💾 [SISTEMA] Transaccion guardada permanentemente en MySQL.");
 
